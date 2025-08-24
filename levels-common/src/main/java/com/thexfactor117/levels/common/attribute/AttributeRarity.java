9faf573c7f961d2891c58d6bedfbca442f367b56
Intybyte <vaan.testwork@gmail.com>
2025-08-24 21:19:56 +0200

    Strip AttributeRarity to only used stuff

--- levels-common/src/main/java/com/thexfactor117/levels/common/attribute/AttributeRarity.java
+++ levels-common/src/main/java/com/thexfactor117/levels/common/attribute/AttributeRarity.java
@@ -1,26 +1,17 @@
 package com.thexfactor117.levels.common.attribute;
 
-import com.thexfactor117.levels.common.LegacyTextColor;
 import lombok.Getter;
 
 @Getter
 public enum AttributeRarity {
-    UNCOMMON(LegacyTextColor.DARK_GREEN, 0x00AA00, 1, 0xF0100010, 0x5000AA00),
-    RARE(LegacyTextColor.AQUA, 0x55FFFF, 2, 0xF0100010, 0x5055FFFF),
-    LEGENDARY(LegacyTextColor.DARK_PURPLE, 0xAA00AA, 3, 0xF0100010, 0x50AA00AA),
+    UNCOMMON(1),
+    RARE(2),
+    LEGENDARY(3),
     ;
 
-    private final String color;
-    private final int hex;
     private final int cost;
-    private final int backColor;
-    private final int borderColor;
 
-    AttributeRarity(Object color, int hex, int cost, int backColor, int borderColor) {
-        this.color = color.toString();
-        this.hex = hex;
+    AttributeRarity(int cost) {
         this.cost = cost;
-        this.backColor = backColor;
-        this.borderColor = borderColor;
     }
 }

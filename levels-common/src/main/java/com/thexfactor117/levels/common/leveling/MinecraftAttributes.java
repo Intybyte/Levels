b9806a5b4253da4824bb9c5d9f32b2ae28699c91
Intybyte <vaan.testwork@gmail.com>
2025-08-26 11:29:20 +0200

    Fix key mistake

--- levels-common/src/main/java/com/thexfactor117/levels/common/leveling/MinecraftAttributes.java
+++ levels-common/src/main/java/com/thexfactor117/levels/common/leveling/MinecraftAttributes.java
@@ -9,7 +9,7 @@ public class MinecraftAttributes {
     public static final String ATTACK_DAMAGE_KEY = "levels:attack_damage";
     public static final String ATTACK_SPEED_KEY = "levels:attack_speed";
     public static final String ARMOR_KEY = "levels:armor";
-    public static final String ARMOR_TOUGHNESS_KEY = "levels_toughness";
+    public static final String ARMOR_TOUGHNESS_KEY = "levels:armor_toughness";
 
     public static final UUID ATTACK_DAMAGE_UUID = UUID.fromString("38d403d3-3e25-4638-957f-71cd25273933");
     public static final UUID ATTACK_SPEED_UUID = UUID.fromString("106410b5-3fa8-4fcf-8252-ca4292dc0391");

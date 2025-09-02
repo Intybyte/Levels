cfe9571774ee70d573053b57cba3bd299816c05c
Intybyte <vaan.testwork@gmail.com>
2025-09-02 23:48:28 +0200

    Levels init for common

--- architectury/1.16.5/common/src/main/java/com/thexfactor117/levels/architectury/Levels.java
+++ architectury/1.16.5/common/src/main/java/com/thexfactor117/levels/architectury/Levels.java
@@ -1,9 +1,16 @@
 package com.thexfactor117.levels.architectury;
 
+import com.thexfactor117.levels.architectury.init.ModEvents;
+import com.thexfactor117.levels.architectury.network.PacketAttributeSelection;
+import com.thexfactor117.levels.common.leveling.ItemType;
+
 public final class Levels {
     public static final String MOD_ID = "levels";
 
     public static void init() {
-        // Write common init code here.
+        ItemType.init();
+        ModEvents.init();
+
+        PacketAttributeSelection.register();
     }
 }

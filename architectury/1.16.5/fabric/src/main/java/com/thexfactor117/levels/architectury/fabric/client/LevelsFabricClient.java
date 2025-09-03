fd1984ca33f7f803353d479d7a8f2e71b749b103
Intybyte <vaan.testwork@gmail.com>
2025-09-03 18:36:59 +0200

    Start fabric implementation

--- architectury/1.16.5/fabric/src/main/java/com/thexfactor117/levels/architectury/fabric/client/LevelsFabricClient.java
+++ architectury/1.16.5/fabric/src/main/java/com/thexfactor117/levels/architectury/fabric/client/LevelsFabricClient.java
@@ -1,10 +1,11 @@
 package com.thexfactor117.levels.architectury.fabric.client;
 
+import com.thexfactor117.levels.architectury.keybindings.Keybinds;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class LevelsFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        Keybinds.register();
     }
 }
